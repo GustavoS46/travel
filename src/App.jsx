da17562ec398f42d65605507206421c34f2b908d
GustavoS46 <gustavooliveirasilva37@gmail.com>
2024-05-21 18:56:43 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import About from './components/About'
+import Contact from './components/Contact'
+import Destination from './components/Destination'
+import Features from './components/Features'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+
+      <main>
+        <div id='home'>
+          <Home />
+        </div>
+
+        <div id='features'>
+          <Features />
+        </div>
+
+        <div id='destination'>
+          <Destination />
+        </div>
+
+        <div id='about'>
+          <About />
+        </div>
+
+        <div id='contact'>
+          <Contact />
+        </div>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Button from "../layouts/Button";
+import { Link } from "react-scroll";
+import img from "../assets/img/about.jpg";
+
+const About = () => {
+    const backgroundColor = `bg-white`;
+  return (
+    <div className="min-h-screen flex flex-col justify-center lg:flex-row items-center md:px-32 px-5 bg-darkBackground gap-10">
+      <div className="w-full lg:w-2/4 space-y-5">
+        <h1 className="font-semibold text-4xl text-white leading-tight">
+          At TripPixie, we are ready to provide you with the best trip of your
+          life!
+        </h1>
+        <p className="text-[#BDBDBD] pb-5">Your dream vacation is waiting for you and we are here to make it come true.</p>
+
+        <Link to="contact" spy={true} smooth={true} duration={500}>
+        <Button title="Contact Now" backgroundColor={backgroundColor} />
+        </Link>
+      </div>
+      <div className="w-full lg:w-2/4">
+        <img className="rounded-2xl" src={img} alt="img" />
+      </div>
+    </div>
+  );
+};
+
+export default About;
